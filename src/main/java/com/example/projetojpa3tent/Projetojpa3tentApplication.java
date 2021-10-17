46f07f1e69c674cea61fcc0d61b7a094cade64f1
lucabaldusca <lucasbaldusca@hotmail.com>
2021-10-17 01:03:30 -0300

    First Commit

---
+++ src/main/java/com/example/projetojpa3tent/Projetojpa3tentApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetojpa3tent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetojpa3tentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetojpa3tentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetojpa3tent/Projetojpa3tentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetojpa3tent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetojpa3tentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
