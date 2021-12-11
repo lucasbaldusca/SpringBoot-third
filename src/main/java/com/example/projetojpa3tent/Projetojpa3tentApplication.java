507401c7ad64e3707f5a66fc6b5da6dd8efd4d8d
lucasbaldusca <lucasbaldusca@hotmail.com>
2021-12-11 01:01:28 -0300

    instantiation operation

--- src/main/java/com/example/projetojpa3tent/Projetojpa3tentApplication.java
+++ src/main/java/com/example/projetojpa3tent/Projetojpa3tentApplication.java
@@ -1,13 +1,29 @@
 package com.example.projetojpa3tent;
 
+import com.example.projetojpa3tent.domain.Categories;
+import com.example.projetojpa3tent.repositories.CategoriesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Projetojpa3tentApplication {
+import java.util.Arrays;
 
+@SpringBootApplication
+public class Projetojpa3tentApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriesRepository categoriesRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(Projetojpa3tentApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categories cat1 = new Categories(null,"Informatica");
+		Categories cat2 = new Categories(null,"Escritorio");
+
+		categoriesRepository.saveAll(Arrays.asList(cat1,cat2));
+
+	}
 }
